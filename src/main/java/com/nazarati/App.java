191ecd1cee94f20a3a62624fb9b68c867c81d3c7
max-nazarati <53535394+max-nazarati@users.noreply.github.com>
2023-06-15 20:58:15 +0200

    fun-16 WIP

--- src/main/java/com/nazarati/App.java
+++ src/main/java/com/nazarati/App.java
@@ -16,22 +16,6 @@ public class App
 
     public static void main(String[] args) throws UnknownHostException
     {
-//        Properties config = new Properties();
-//        config.put("client.id", InetAddress.getLocalHost().getHostName());
-//        config.put("group.id", "test-consumer-group");
-//        config.put("bootstrap.servers","localhost:9092");
-//        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-//        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-//
-//        var consumer = new KafkaConsumer<String, String>(config);
-//        consumer.subscribe(List.of("test-messages"));
-//        System.out.println("SERVER INITIALISED");
-//
-//        while(true) {
-//            var records = consumer.poll(Duration.ofMillis(500));
-//            records.forEach(x -> System.out.println(x.value()));
-////            System.out.println("===");
-//        }
         SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/nazarati/config/MessageProcessor.java
+++ src/main/java/com/nazarati/config/MessageProcessor.java
@@ -1,15 +1,63 @@
 package com.nazarati.config;
 
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.AerospikeException;
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
+import com.aerospike.client.Log;
+import com.aerospike.client.Operation;
+import com.aerospike.client.Value;
+import com.aerospike.client.cdt.ListOperation;
+import com.aerospike.client.cdt.ListOrder;
+import com.aerospike.client.cdt.ListPolicy;
+import com.aerospike.client.cdt.ListWriteFlags;
+import com.aerospike.client.policy.ClientPolicy;
+import com.aerospike.client.policy.WritePolicy;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageProcessor {
 
+    int counter = 0;
+    String userA = "a";
+    String userB = "b";
+    String lastSenderName = userA;
+
     @KafkaListener(id = "message-listener", topics = "messages")
     public void listen(String data) {
-        System.out.println(data);
-        System.out.println();
+        try {
+            System.out.println(data);
+
+            Log.setCallbackStandard();
+            var policy = new ClientPolicy();
+            policy.user = "admin";
+            policy.password = "admin";
+
+            var client = new AerospikeClient("127.0.0.1", 3000);
+            var writePolicy = new WritePolicy();
+            writePolicy.sendKey = true;
+
+            var listPolicy = new ListPolicy(ListOrder.ORDERED, ListWriteFlags.INSERT_BOUNDED);
+
+            var key = new Key("tinychat", "messages", counter++);
+
+            var lastSender = new Bin("last_sender", lastSenderName);
+
+            lastSenderName = lastSenderName.equals(userA) ? userB : userA;
+
+            var x = client.operate(writePolicy,
+                    key,
+                    Operation.put(lastSender),
+                    ListOperation.append(listPolicy, "msg_chain", Value.get(data)),
+                    Operation.get()
+            );
+            System.out.println();
+            client.close();
+        }
+        catch (AerospikeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
